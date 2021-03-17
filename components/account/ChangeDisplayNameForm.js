6433de81ba624e8cf03c1bcaa666f3d8af5f7c6e
SantiagoBetancurCardona97 <santibetancur97@gmail.com>
2021-03-17 06:56:39 -0500

    add
    Video 55

--- components/account/ChangeDisplayNameForm.js
+++ components/account/ChangeDisplayNameForm.js
@@ -1,14 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, View } from 'react-native'
 import {Button, Input} from 'react-native-elements'
+import { isEmpty } from 'lodash'
 
-export default function ChangeDisplayNameForm({displayName, setShowModal, toastRef}) {
+import { updateProfile } from '../../utils/actions'
+
+export default function ChangeDisplayNameForm({displayName, setShowModal, toastRef, setRelodUser}) {
+    const [newDisplayName, setNewDisplayName] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    const onSubmit= async() =>{
+        if (!validateForm()){
+            return    
+        }
+        
+        setLoading(true)
+        const result = await updateProfile ({displayName: newDisplayName})
+        setLoading(false)
+
+        if(!result.statusResponse){
+            setError("Error al actualizar Nombres y Apellidos, intenta mas tarde.")
+            return
+        }
+
+        setRelodUser(true)
+        toastRef.current.show("Se han actualizado Nombres yApellidos", 3000)
+        setShowModal(false)
+    }
+
+    const validateForm =() => {
+        setError(null)
+
+        if (isEmpty(newDisplayName)) {
+            setError("Debes ingresar Nombres y Apellidos.")
+            return false
+        }
+        if (newDisplayName === displayName) {
+            setError("Debes ingresar Nombres y Apellidos, diferentes de los actuales.")
+            return false
+        }
+        return true
+    }
     return (
         <View style={styles.view}>
             <Input
                 placeholder="Ingresa nombres y apellidos"
                 containerStyle={styles.iput}
                 defaultValue={displayName}
+                onChange={(e) => setNewDisplayName(e.nativeEvent.text)}
+                errorMessage={error}
                 rightIcon={{
                     type:"material-community",
                     name:"account-circle-outline",
@@ -19,6 +60,8 @@ export default function ChangeDisplayNameForm({displayName, setShowModal, toastR
                 title="Cambiar Nombre y Apellidos"
                 containerStyle={styles.btnContainer}
                 buttonStyle={styles.btn}
+                onPress={onSubmit}
+                loading={loading}
             />
 
         </View>
