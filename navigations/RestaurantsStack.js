e5e626c50a6263c12e9d2411bdf3b5932c1ac460
SantiagoBetancurCardona97 <santibetancur97@gmail.com>
2021-03-22 21:30:24 -0500

    Add
    Video 61

--- navigations/RestaurantsStack.js
+++ navigations/RestaurantsStack.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React  from 'react'
 
 import {createStackNavigator} from '@react-navigation/stack'
-import Restaurants from '../screens/Restaurants'
+import Restaurants from '../screens/restaurants/Restaurants'
+import AddRestaurant from '../screens/restaurants/AddRestaurant'
 
 const Stack =  createStackNavigator()
 
@@ -12,6 +13,11 @@ export default function RestaurantsStack() {
                 name="restaurants"
                 component={Restaurants}
                 options={{ title: "Restaurantes" }}            
+            />
+            <Stack.Screen
+                name="add-restaurants"
+                component={AddRestaurant}
+                options={{ title: "Crear un nuevo Restaurante" }}            
             />
         </Stack.Navigator>
     )
